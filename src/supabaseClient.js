71104fdabe391f6655f92fcfc92f1796f2295412
Amal-nellanhi <amalnellanhi@gmail.com>
2025-12-20 23:01:15 +0530

    Add supabase connecting code and linked the keys

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = "https://omqrlshawfaqhcpqjflb.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tcXJsc2hhd2ZhcWhjcHFqZmxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyNDI1NDAsImV4cCI6MjA4MTgxODU0MH0.YH_sonP1DhEbHgUEIzGlmhepf24Af-kV0jG3cdAdaHU";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
